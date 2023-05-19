d0a7ac39acaa5ae3b53587ec7cb9aa953724a63e
Adrianominora <adriano.minora@mail.polimi.it>
2023-05-19 18:10:26 +0200

    lot of stuff

--- script/Forecast_previus.py
+++ script/Forecast_previus.py
@@ -108,13 +108,14 @@ def compute_start(new_pos, window_size=7):
 
 # Single wave fit
 class wave_fit:
-    def __init__(self, new_pos_tot, params = None) :
-        if param == None:
-            self.params = make_params_0()
-        else:
-            self.params = params
-
+    def __init__(self, new_pos_tot) :
+        self.param_list=[]
+        self.forecast_list=[]
+        self.corrected_forecast_list=[]
+        self.conc_list=[]
+        self.fit_list=[]
         self.new_pos_tot=new_pos_tot
+        self.new_pos_avg = 0
         self.norm_factor = np.amax(new_pos_tot)
 
     def make_forecast(new_pos, params, T = 14,max_nfev=10000):
@@ -143,112 +144,122 @@ class wave_fit:
 
         return prediction
         
-    def fit(self,conc_flag, max_nfev = 1000) :
-        t0=0
-        T_tot = new_pos_tot.shape[0]
+    def fit(self,t0,T_tot):
         if not(T_tot>t0):
             print('Final time has to be greater than initial time')
             raise
-    
-        new_pos = self.new_pos_tot[t0-14:t]  # Data acquisition
-        self.new_pos_avg = rolling_avg(new_pos)
+        for t in range(max(t0,14),T_tot):
+            new_pos = self.new_pos_tot[t0-14:t]  # Data acquisition
+            self.new_pos_avg = rolling_avg(new_pos)
 
-        
-        
-        # Create model
-        fmodel= Model(gompertz)
-        # fit the model
-        result = fmodel.fit(self.new_pos_tot/self.norm_factor, self.params, 
-                            t=np.arange(t0,T_tot), max_nfev=max_nfev)
+            daily_err = compute_derr(new_pos,self.new_pos_avg)
+            # daily_err = np.load('../data/daily_errors.npy')[(t+1-3)%7]
 
-        # Update
-        self.params = result.params
+            # Compute concavity
+            self.conc_list.append(copute_conc(self.new_pos_avg))
 
-    def set_params(params):
-        self.params=params
-        
-    @ property
-    def param_list(self):
-        return self.param_list
-    
-    @ property
-    def forecast_list(self):
-        return self.forecast_list
-    
-    @ property
-    def corrected_forecast_list(self):
-        return self.corrected_forecast_list
-    
-    @ property
-    def conc_list(self):
-        return self.conc_list
-    
-    @ property
-    def fit_list(self):
-        return self.fit_list
-    
-    @ property
-    def new_pos_avg(self):
-        return self.new_pos_avg
-    
-    @ property
-    def norm_factor(self):
-        return self.norm_factor
-
-# Multiple waves fit
-class n_waves:
-    def __init__(self, new_pos_tot):
-        self.new_pos_tot=new_pos_tot
-        self.L_waves = [0]
-        self.L_starts = [0]
-        self.n = 1
-
-    def fit(self, T):
-
-        t0=0
-        for t in range (14,T):
-            t_start = compute_start(self.new_pos_tot[0:t], window_size=7)
-            t_start_new = t_start[-1]
-
-
-            if(self.n==1):
-                if self.L_starts[-1] != t_start_new:
-                    self.L_starts.append(t_start_new)
-                    self.n += 1
-
-                    
-                    WF = wave_fit(self.new_pos_tot[0,t])
-                    WF.fit(t,t+1)
-                    self.L_waves.append(WF)
-
-                else:
-                    WF = wave_fit(self.new_pos_tot[0,t])
-                    WF.fit(t,t+1)
-                    self.L_waves[self.n-1] = WF
+            # Correct correction
+            if t>t0+24:
+                daily_err += 75*self.conc_list[-1]
             
+            # Define parameters
+            if len(param_list)==0:
+                params = make_param_0()
             else:
-                if self.L_starts[-1] != t_start_new:
-                    self.L_starts.append(t_start_new)
-                    self.n += 1
-                    
-                    new_pos_t = self.new_pos_tot[self.L_starts[-1]  , t]
+                params = param_list[-1]
+            
+            fit, result_params, forecast= make_forecast(self.new_pos_avg, params)
+            
+            # Saving parameters and forecast
+            self.param_list.append(result_params)
+            self.forecast_list.append(forecast)
+            self.corrected_forecast_list.append(forecast*(1+daily_err))
+            self.fit_list.append(fit)
 
-                    last_params = self.L_waves[-1].param_list[-1]
+        def set_params(params):
+            self.param_list=[params]
+        
+        @ property
+        def param_list(self):
+            return self.param_list
+        
+        @ property
+        def forecast_list(self):
+            return self.forecast_list
+        
+        @ property
+        def corrected_forecast_list(self):
+            return self.corrected_forecast_list
+        
+        @ property
+        def conc_list(self):
+            return self.conc_list
+        
+        @ property
+        def fit_list(self):
+            return self.fit_list
+        
+        @ property
+        def new_pos_avg(self):
+            return self.new_pos_avg
+        
+        @ property
+        def norm_factor(self):
+            return self.norm_factor
+        
+    class n_waves:
+        def __init__(self, new_pos_tot):
+            self.new_pos_tot=new_pos_tot
+            self.L_waves = [0]
+            self.L_starts = [0]
+            self.n = 1
 
-                    fit = self.L_waves[-1].norm_factor*gompertz(np.arange(0, self.L_starts[-1] - self.L_starts[-2]), params['K'].value, params['b'].value, params['c'].value)
-                    forecast = fit[today+1]
-                    diff_pos = new_pos_t - make_forecast(new_pos_t,self.L_waves[-1].param_list[-1], t )
+        def fit(self, T):
+
+            t0=0
+            for t in range (14,T):
+                t_start = compute_start(self.new_pos_tot[0:t], window_size=7)
+                t_start_new = t_start[-1]
 
 
-                    WF = wave_fit(self.new_pos_tot[self.L_starts[-1],t] - )
-                    WF.fit(t,t+1)
-                    self.L_waves.append(WF)
+                if(self.n==1):
+                    if self.L_starts[-1] != t_start_new:
+                        self.L_starts.append(t_start_new)
+                        self.n += 1
 
+                        
+                        WF = wave_fit(self.new_pos_tot[0,t])
+                        WF.fit(t,t+1)
+                        self.L_waves.append(WF)
+
+                    else:
+                        WF = wave_fit(self.new_pos_tot[0,t])
+                        WF.fit(t,t+1)
+                        self.L_waves[self.n-1] = WF
+                
                 else:
-                    WF = wave_fit(self.new_pos_tot[self.L_starts[-1]-14,t])
-                    WF.fit(t,t+1)
-                    self.L_waves[self.n-1] = WF
+                    if self.L_starts[-1] != t_start_new:
+                        self.L_starts.append(t_start_new)
+                        self.n += 1
+                        
+                        new_pos_t = self.new_pos_tot[self.L_starts[-1]  , t]
+
+                        last_params = self.L_waves[-1].param_list[-1]
+
+                        fit = self.L_waves[-1].norm_factor*gompertz(np.arange(0, self.L_starts[-1] - self.L_starts[-2]), params['K'].value, params['b'].value, params['c'].value)
+                        forecast = fit[today+1]
+                        diff_pos = new_pos_t - make_forecast(new_pos_t,self.L_waves[-1].param_list[-1], t )
 
 
-            forecast_list+=WF.forecast_list[0]
-            corrected_forecast_list = WF.corrected_forecast_list
+                        WF = wave_fit(self.new_pos_tot[self.L_starts[-1],t] - )
+                        WF.fit(t,t+1)
+                        self.L_waves.append(WF)
+
+                    else:
+                        WF = wave_fit(self.new_pos_tot[self.L_starts[-1]-14,t])
+                        WF.fit(t,t+1)
+                        self.L_waves[self.n-1] = WF
+
+
+                forecast_list+=WF.forecast_list[0]
+                corrected_forecast_list = WF.corrected_forecast_list
