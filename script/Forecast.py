60ddf70644832876aa059f737918670d5e3b0b22
Adrianominora <adriano.minora@mail.polimi.it>
2023-05-25 15:19:22 +0200

    committed

--- script/Forecast.py
+++ script/Forecast.py
@@ -16,15 +16,16 @@ def cum_gompertz(t, K, b, c):
 
 def make_param_0():
     params = Parameters()
-    params.add("K", 10, min=0, max=100)
-    params.add("b", 1, min=1e-2, max=100)
-    params.add("c", 1, min=1e-6, max=2)
+    params.add("K", 10, min=5, max=100)
+    params.add("b", 1, min=1, max=100)
+    params.add("t_max", 20, min=15, max=100)
+    params.add("c", 1, expr='log(b)/t_max')
     #params.add(f"d", 0, min=0)
 
     return params
 
 def compute_rt(func, window_size=14):
-    data = func[-(window_size+1):-1]
+    data = func[-(window_size):]
     log_data = np.log(data)
     rt = np.zeros((log_data.shape[0]-1,))
     for i in range(rt.shape[0]):
@@ -81,7 +82,7 @@ def copute_conc(func, window_size=14):
     return np.mean(concv)
 
 def compute_start(new_pos, window_size=7, start_0=14):
-    tt=np.arange(0,new_pos.shape[0])
+    tt=np.arange(new_pos[0],new_pos.shape[0])
     rt = np.zeros(new_pos.shape)
     new_pos_fit=np.zeros(new_pos.shape)
     for t in range(window_size,tt[-1]):
@@ -107,41 +108,32 @@ def compute_start(new_pos, window_size=7, start_0=14):
 class wave_fit:
     norm_factor = 1
     params = make_param_0()
-    # def __init__(self) :
-        # if params == None:
-        #     self.params = Parameters()
-        #     self.params = make_param_0()
-        # else:
-        #     self.params = Parameters()
-        #     self.params = params
-        
-
-    # def make_param_0(self):
-    #     self.params.add(f"K", 10, min=0, max=100)
-    #     self.params.add(f"b", 1, min=1e-2, max=100)
-    #     self.params.add(f"c", 1, min=1e-6, max=2)
-    #     #params.add(f"d", 0, min=0)
-
-    # def set_norm_factor(self, nf):
-    #     self.norm_factor(nf)
     
     def predict(self, tt):
-        prediction = self.norm_factor*gompertz(tt, self.params['K'].value, self.params['b'].value, self.params['c'].value)
+        prediction = self.norm_factor*gompertz(tt, self.params['K'].value, self.params['b'].value , self.params['c'].value)
         return prediction
         
-    def fit(self, new_pos, max_nfev = 1000) :
+    def fit(self, new_pos, max_nfev = 1000, window=1):
         self.norm_factor = np.amax(new_pos)
         t0=0
         T_tot = new_pos.shape[0]
         if not(T_tot>t0):
             print('Final time has to be greater than initial time')
             raise
-        new_pos_avg = rolling_avg(new_pos)
         # Create model
         fmodel= Model(gompertz)
         # fit the model
-        result = fmodel.fit(new_pos/self.norm_factor, self.params, 
-                            t=np.arange(t0,T_tot), max_nfev=max_nfev)
+        if window<1:
+            print('Invalid time window for interpreting data')
+            raise
+        elif window==1:
+            result = fmodel.fit(new_pos/self.norm_factor, self.params, 
+                                t=np.arange(t0,T_tot), max_nfev=max_nfev)
+        else:
+            new_pos_avg = rolling_avg(new_pos, window_size=window)
+            result = fmodel.fit(new_pos_avg/self.norm_factor, self.params, 
+                                t=np.arange(t0,T_tot)-window/2, max_nfev=max_nfev)
+
         # Update
         self.params = result.params
 
@@ -150,31 +142,64 @@ class wave_fit:
 
 # Multiple waves fit
 class n_waves:
-    def __init__(self, new_pos_tot):
+    def __init__(self, new_pos_tot, window=1):
         self.new_pos_tot=new_pos_tot
         self.L_waves = [wave_fit()]
         self.n = 1
         self.overlap = 14
         self.L_starts = [self.overlap]
+        self.L_ends = [50]
+        self.window = window
+        self.t=self.overlap - 1
+        self.rt = 0
 
+    def update_tstart(self, poly_w=7): #"""controlla"""
+        flag = False
+        new_pos_fit = np.zeros(poly_w)
+        tt = np.arange(self.t-poly_w, self.t)
+        for i in range(poly_w):
+            t = tt[i]
+            coefficients = np.polyfit(np.arange(t - poly_w, t), self.new_pos_tot[t - poly_w: t], 0)
+            new_pos_fit[i] = np.polyval(coefficients, t)
+        rt_fit = compute_rt(self.predict(tt))
+        self.rt = compute_rt(new_pos_fit, window_size=poly_w) -rt_fit
+
+        # if rt>0.04 and self.t>self.L_ends[-1] and self.t>self.L_starts[-1]+40 and len(self.L_ends)==len(self.L_starts):
+        #     self.L_starts.append(self.t)
+        #     flag = True
+        # if rt<-0.025 and self.t>self.L_starts[-1]+20 and self.t>self.L_ends[-1]+40 and len(self.L_ends)==len(self.L_starts)-1:
+        #     self.L_ends.append(self.t)
+        if self.rt>0.04 and self.t>self.L_starts[-1]+40 :
+            self.L_starts.append(self.t)
+            flag = True
+        return flag
+
+
+    def daily_update(self):
+        self.t +=1
+        t = self.t
+        new_wave_flag = self.update_tstart()
+
+        if new_wave_flag:
+            WF = wave_fit()
+            self.L_waves.append(WF)
+            self.n += 1
+
+        if(self.n==1):
+            self.L_waves[-1].fit(self.new_pos_tot[0:t], window=self.window)
+            return self.new_pos_tot[t]
+        else:
+            new_pos_t = self.new_pos_tot[self.L_starts[-1] - self.overlap  : t]
+            # tail_pos = self.L_waves[-2].predict(np.arange(self.L_starts[-1] - self.L_starts[-2], t - (self.L_starts[-2] - self.overlap)))
+            tail_pos = self.predict(np.arange(self.L_starts[-1] - self.overlap  , t), self.n-1 )
+            self.L_waves[-1].fit(new_pos_t-tail_pos, window=self.window)
+            return (new_pos_t-tail_pos)[-1]
+        
     def fit(self, T):
         for t in range (self.overlap,T):
-            t_start = compute_start(self.new_pos_tot[0:t], window_size=7, start_0=self.overlap)
-            t_start_new = t_start[-1]
+            self.daily_update()
 
-            if self.L_starts[-1] != t_start_new:
-                WF = wave_fit()
-                self.L_waves.append(WF)
-                self.L_starts.append(t_start_new)
-                self.n += 1
-
-            if(self.n==1):
-                self.L_waves[-1].fit(self.new_pos_tot[0:t])
-            else:
-                new_pos_t = self.new_pos_tot[self.L_starts[-1] - self.overlap  : t]
-                tail_pos = self.L_waves[-1].predict(np.arange(self.L_starts[-1] - self.L_starts[-2], t - (self.L_starts[-2] - self.overlap)))
-                self.L_waves[-1].fit(new_pos_t-tail_pos)
-
+            
     def single_predict(self, wave_index, tt):
         """This return a single wave contribution in a given interval expressed as global time"""
         if wave_index >= self.n:
@@ -184,12 +209,49 @@ class n_waves:
         tt_local = tt - (self.L_starts[wave_index] - self.overlap)
         return wave.predict(tt_local)
 
-    def predict(self, tt):
+    def predict(self, tt, n=None):
         """This return the current prediction considering all waves contributions"""
+        if n==None:
+            n=self.n
         sum = np.zeros(tt.shape)
-        for i in range(self.n):
+        for i in range(n):
             sum += self.single_predict(i, tt)
         return sum
+        # sum = np.zeros(tt.shape)
+        # for j in range(tt.shape[0]):
+        #     t = tt[j]
+        #     last_wave_index=0
+        #     while self.L_starts[last_wave_index]<t:
+        #         last_wave_index+=1
+        #     last_wave_index-=1
+        #     if last_wave_index==-1:
+        #         last_wave_index=0
 
-    # forecast_list+=WF.forecast_list[0]
-    # corrected_forecast_list += WF.corrected_forecast_list[0]
+        #     for i in range(last_wave_index):
+        #         sum[j] += self.single_predict(i, tt)
+        # return sum
+
+
+
+
+    # def fit_and_predict(self, T, tt):
+    #     jj = 0
+    #     for t in range (self.overlap,T):
+    #         tt_prediction = tt+t
+    #         t_start = compute_start(self.new_pos_tot[0:t], window_size=7, start_0=self.overlap)
+    #         t_start_new = t_start[-1]
+
+    #         if self.L_starts[-1] != t_start_new:
+    #             WF = wave_fit()
+    #             self.L_waves.append(WF)
+    #             self.L_starts.append(t_start_new)
+    #             self.n += 1
+
+    #         if(self.n==1):
+    #             self.L_waves[-1].fit(self.new_pos_tot[0:t], window=self.window)
+    #         else:
+    #             new_pos_t = self.new_pos_tot[self.L_starts[-1] - self.overlap  : t]
+    #             tail_pos = self.L_waves[-1].predict(np.arange(self.L_starts[-1] - self.L_starts[-2], t - (self.L_starts[-2] - self.overlap)))
+    #             self.L_waves[-1].fit(new_pos_t-tail_pos, window=self.window)
+    #         prediction[jj] = self.predict(tt_prediction)
+    #         jj+=1
